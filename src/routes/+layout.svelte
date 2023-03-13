28c97d1a714de097f0a0a1f4de210164ec44d054
Andrey Ilin <andrey@passbase.com>
2023-03-13 22:44:09 +0100

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+	import './styles.css';
+
+	const year = new Date().getFullYear();
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+
+	<footer>{year}, Andrey Ilin</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		height: 100vh;
+		margin: 0 3rem;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-width: 64rem;
+		margin: 3rem auto 0;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		padding: 3rem 4rem;
+		margin: 0 auto;
+	}
+</style>
