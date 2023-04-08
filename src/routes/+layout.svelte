e3dfaff76d1bdc74461afe3bcae466e50b9a953c
Andrey Ilin <neform@gmail.com>
2023-04-08 19:40:16 +0200

    feat: added dark theme refactor: improved layout, changed content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,40 +1,94 @@
 <script>
 	import './styles.css';
 
-	const year = new Date().getFullYear();
+	let links = [
+		{ title: 'GitHub', url: 'https://github.com/ilin-andrey' },
+		{ title: 'LinkedIn', url: 'https://www.linkedin.com/in/andrey-ilin/' }
+	];
 </script>
 
+<svelte:head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width" />
+	<title>Andrey Ilin</title>
+	<meta name="description" content="Personal website" />
+</svelte:head>
+
 <div class="app">
+	<header>
+		<h2>Hey! I'm Andrey.</h2>
+	</header>
+
 	<main>
 		<slot />
 	</main>
 
-	<footer>{year}, Andrey Ilin</footer>
+	<footer>
+		<ul>
+			{#each links as link}
+				<li>
+					<a href={link.url} target="_blank">{link.title}</a>
+				</li>
+			{/each}
+		</ul>
+	</footer>
 </div>
 
 <style>
-	.app {
+	header {
 		display: flex;
-		flex-direction: column;
 		justify-content: space-between;
-		height: 100vh;
-		margin: 0 3rem;
+		align-content: center;
+		gap: 1rem;
+		margin: 0 0 3rem 0;
 	}
 
 	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-		max-width: 64rem;
-		margin: 3rem auto 0;
 		box-sizing: border-box;
 	}
 
 	footer {
+		max-width: 64rem;
+		padding: 0;
+		margin: 3rem 0;
+	}
+
+	ul {
 		display: flex;
-		flex-direction: column;
-		padding: 3rem 4rem;
+		flex-direction: row;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	li {
+		white-space: nowrap;
+	}
+
+	li::after {
+		margin: 0 0.5em;
+		content: '·';
+	}
+
+	li:last-child::after {
+		content: '';
+		margin: 0;
+	}
+
+	@media (max-width: 260px) {
+		ul {
+			flex-direction: column;
+			gap: 0.5rem;
+		}
+
+		li::after {
+			content: '';
+		}
+	}
+
+	.app {
+		max-width: 56rem;
 		margin: 0 auto;
+		padding: 3rem 2rem 0;
 	}
 </style>
